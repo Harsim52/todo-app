0d0c5d6adf81c5c33ab54e9b870e946ce02e4086
Harsim52 <Harsim52@gmail.com>
2025-02-26 00:17:34 +0530

    Initial commit

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const TodoInput = () => {
+
+const [input, setInput] = useState(null);
+
+
+  return (
+    <div>
+        <input type="text" />
+    </div>
+  )
+}
+
+export default TodoInput
